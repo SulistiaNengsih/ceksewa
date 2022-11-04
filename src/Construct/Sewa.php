d241ccc1261bb57b1049a4acea236ee0937c7961
sulistianengsih5 <sulistianengsih5@gmail.com>
2022-11-04 14:11:56 +0700

    'commit'

--- src/Construct/Sewa.php
+++ src/Construct/Sewa.php
@@ -5,19 +5,15 @@ require './Entity/Produk.php';
 class Sewa implements PenyewaanBarang {
   public $id_sewa;
   public $produk;
-  public $status;
-  public $tanggal_mulai_sewa;
-  public $tanggal_akhir_sewa;
+  public $statusPengembalian;
+  public $lama_sewa;
   public $durasi_sewa;
   public $denda;
 
-  public __construct($id_sewa, $id_produk, $nama_produk, $biaya_sewa, $status, $tanggal_mulai_sewa,
-                      $tanggal_akhir_sewa, $durasi_sewa, $denda) {
-    $this->id_sewa = $id_sewa;
+  public __construct($id_sewa, $id_produk, $nama_produk, $biaya_sewa, $durasi_sewa, $denda) {
     $this->produk = new Produk($id_produk, $nama_produk, $biaya_sewa);
-    $this->status = $status;
-    $this->tanggal_mulai_sewa = $tanggal_mulai_sewa;
-    $this->tanggal_akhir_sewa = $tanggal_akhir_sewa;
+    $this->id_sewa = $id_sewa;
+    $this->statusPengembalian = false;
     $this->durasi_sewa = $durasi_sewa;
     $this->denda = $denda;
   }
@@ -31,6 +27,9 @@ class Sewa implements PenyewaanBarang {
       calculateDenda();
     }
   }
-}
 
+  public function calculateDenda($biaya_denda = $this->produk->biaya_denda, $durasi_keterlambatan = $this->lama_sewa - $this->durasi_sewa) {
+    return $biaya_denda * $durasi_keterlambatan;
+  }
+}
  ?>
